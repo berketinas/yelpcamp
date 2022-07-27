e01fb7df1d5f22c9685f022613ad55e5c660350c
Berke Tınas <berke_750@hotmail.com>
2022-07-27 14:46:21 +0300

    added new custom error class and a wrapper to catch all errors in async funcctions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const engine = require('ejs-mate');
+
+const asyncWrapper = require('./utils/asyncWrapper');
 const Campground = require('./models/campground');
 
 mongoose.connect(
@@ -33,43 +35,48 @@ app.get('/', (req, res) => {
     res.redirect('/campgrounds');
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', asyncWrapper(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-});
+}));
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', asyncWrapper(async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', asyncWrapper(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/detail', { campground });
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', asyncWrapper(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${id}`);
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', asyncWrapper(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', asyncWrapper(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit', { campground });
+}));
+
+// eslint-disable-next-line
+app.use((err, req, res, next) => {
+    res.send('something went wrong.');
 });
 
 app.listen(3000, () => {
