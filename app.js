ea32c1f5a6dd0230b1b0f06f6963cb15ca794a12
Berke Tınas <berke_750@hotmail.com>
2022-07-27 10:46:49 +0300

    added layout directory and boilerplate file for all pages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const engine = require('ejs-mate');
 const Campground = require('./models/campground');
 
 mongoose.connect(
@@ -20,6 +21,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
