879048c5012814943d4359c2ccb575a54f59ddf4
Berke Tınas <berke_750@hotmail.com>
2022-08-01 11:42:51 +0300

    added multer

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const multer = require('multer');
 const asyncWrapper = require('../utils/asyncWrapper');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const CampgroundController = require('../controllers/campgrounds');
 
 const router = express.Router();
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/')
     .get(asyncWrapper(CampgroundController.index))
-    .post(isLoggedIn, validateCampground, asyncWrapper(CampgroundController.create));
+    // .post(isLoggedIn, validateCampground, asyncWrapper(CampgroundController.create));
+    .post(upload.array('image'), (req, res) => {
+        res.send(req.files);
+    });
 
 router.get('/new', isLoggedIn, CampgroundController.renderNew);
 
