7e272f848af7a754b0f6de2139daaceaae034530
Berke Tınas <berke_750@hotmail.com>
2022-08-01 16:59:26 +0300

    passed campgrounds data to cluster map script

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -11,7 +11,7 @@ const map = new mapboxgl.Map({
 map.on('load', () => {
     map.addSource('earthquakes', {
         type: 'geojson',
-        data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+        data: campgrounds,
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
